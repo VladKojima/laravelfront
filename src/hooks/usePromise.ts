5d180af984f4576e81e289e3239029deac0a5ab2
VladKojima <VladKojima@users.noreply.github.com>
2024-12-03 22:52:57 +0400

    added hook for promise

---
+++ src/hooks/usePromise.ts
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+type Statuses = "inited" | "pending" | "fulfilled" | "rejected";
+
+export function usePromise<T, P extends unknown[]>(promise: (...args: P) => Promise<T>): [(...props: P) => void, Statuses, T | undefined, Error | undefined] {
+    const [status, setStatus] = useState<Statuses>("inited");
+
+    const [value, setValue] = useState<T | undefined>(undefined);
+    const [error, setError] = useState<Error | undefined>(undefined);
+
+    const start = (...props: P) => {
+        setStatus('pending');
+        setValue(undefined);
+        setError(undefined);
+        promise(...props)
+            .then(result => { setValue(result); setStatus("fulfilled") })
+            .catch(error => { setError(error); setStatus("rejected") });
+    };
+
+    return [start, status, value, error];
+}
