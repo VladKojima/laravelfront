60489615054a0fa4ee18c9afbb029e14306619cb
VladKojima <VladKojima@users.noreply.github.com>
2024-12-03 23:13:17 +0400

    useCallback on usePromise

--- src/hooks/usePromise.ts
+++ src/hooks/usePromise.ts
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
 type Statuses = "inited" | "pending" | "fulfilled" | "rejected";
 
@@ -8,14 +8,14 @@ export function usePromise<T, P extends unknown[]>(promise: (...args: P) => Prom
     const [value, setValue] = useState<T | undefined>(undefined);
     const [error, setError] = useState<Error | undefined>(undefined);
 
-    const start = (...props: P) => {
+    const start = useCallback((...props: P) => {
         setStatus('pending');
         setValue(undefined);
         setError(undefined);
         promise(...props)
             .then(result => { setValue(result); setStatus("fulfilled") })
             .catch(error => { setError(error); setStatus("rejected") });
-    };
+    }, [promise]);
 
     return [start, status, value, error];
 }
