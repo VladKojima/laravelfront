7bb0f22de102a78e6702fbc8eae85ba41a5634aa
VladKojima <VladKojima@users.noreply.github.com>
2024-12-06 23:00:42 +0400

    add datetime selectors

--- src/App.tsx
+++ src/App.tsx
@@ -9,45 +9,50 @@ import { DishesPage } from './pages/dishes';
 import { Page } from './components/page';
 import { ReservationForm } from './pages/reservation';
 import { HallsPage } from './pages/halls';
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import 'dayjs/locale/ru';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Router>
-        <AppBar sx={{ minHeight: "80px", display: 'grid', gridTemplateColumns: "1fr 5fr 1fr", marginBottom: 5 }} position='static'>
-          <Container sx={{ gridColumn: 2, textAlign: "center", alignContent: "center", ">*": { margin: "5px 5px" } }}>
-            <Button variant='contained' color='secondary' component={Link} to="/">Главная</Button>
-            <Button variant='contained' color='secondary' component={Link} to="/dishes">Блюда</Button>
-            <Button variant='contained' color='secondary' component={Link} to="/halls">Залы</Button>
-            <Button variant='contained' color='secondary' component={Link} to="/preservation">Бронирование</Button>
+      <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale='ru'>
+        <Router>
+          <AppBar sx={{ minHeight: "80px", display: 'grid', gridTemplateColumns: "1fr 5fr 1fr", marginBottom: 5 }} position='static'>
+            <Container sx={{ gridColumn: 2, textAlign: "center", alignContent: "center", ">*": { margin: "5px 5px" } }}>
+              <Button variant='contained' color='secondary' component={Link} to="/">Главная</Button>
+              <Button variant='contained' color='secondary' component={Link} to="/dishes">Блюда</Button>
+              <Button variant='contained' color='secondary' component={Link} to="/halls">Залы</Button>
+              <Button variant='contained' color='secondary' component={Link} to="/preservation">Бронирование</Button>
+            </Container>
+            <Toolbar sx={{ justifyContent: "end" }}>
+              <Button
+                variant='contained'
+                color='secondary'
+                endIcon={<Login />}
+                component={Link}
+                to={"/login"}
+              >
+                Вход/Регистрация
+              </Button>
+            </Toolbar>
+
+          </AppBar>
+          <Container>
+            <Page>
+              <Routes>
+                <Route path="/register" element={<RegisterPage />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/dishes" element={<DishesPage />} />
+                <Route path="/halls" element={<HallsPage />} />
+                <Route path="/preservation" element={<ReservationForm />} />
+                <Route path="/*" element={<MainPage />} />
+              </Routes>
+            </Page>
+
           </Container>
-          <Toolbar sx={{ justifyContent: "end" }}>
-            <Button
-              variant='contained'
-              color='secondary'
-              endIcon={<Login />}
-              component={Link}
-              to={"/login"}
-            >
-              Вход/Регистрация
-            </Button>
-          </Toolbar>
-
-        </AppBar>
-        <Container>
-          <Page>
-            <Routes>
-              <Route path="/register" element={<RegisterPage />} />
-              <Route path="/login" element={<LoginPage />} />
-              <Route path="/dishes" element={<DishesPage />} />
-              <Route path="/halls" element={<HallsPage />} />
-              <Route path="/preservation" element={<ReservationForm />} />
-              <Route path="/*" element={<MainPage />} />
-            </Routes>
-          </Page>
-
-        </Container>
-      </Router>
+        </Router>
+      </LocalizationProvider>
     </ThemeProvider>
   );
 }
